91dc5a217e36ff0f3b984ec513f103775ecc9925
Tanner Moore <tdmoore004@gmail.com>
2020-11-19 18:49:36 -0700

    Added schema for work adding working data.

---
+++ models/workout.js
@@ -0,0 +1,49 @@
+// Require in Mongoose
+const mongoose = require("mongoose");
+
+// Set up Mongoose Schema
+const Schema = mongoose.Schema;
+
+// Create new Schema for workout data
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+  exercise: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      weight: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
