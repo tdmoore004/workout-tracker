ec039f021a429e75dfa4dee53aabe57011d0c412
Tanner Moore <tdmoore004@gmail.com>
2020-11-19 19:01:20 -0700

    Added api routes.

---
+++ routes/api-routes.js
@@ -0,0 +1,51 @@
+// Require in express and express router
+const express = require("express");
+const router = express.Router();
+
+// Require in the workout schema.
+const db = require("../models/workout");
+
+// Route for getting all workout data.
+router.get("/", (req, res) => {
+	db.Workout.find({}).then(dbWorkout => {
+		res.json(dbWorkout);
+	}).catch(err => {
+		res.json(err);
+	});
+});
+
+// Route for posting new workout
+router.post("/", ({ body }, res) => {
+	db.Workout.create(body).then((dbWorkout => {
+		res.json(dbWorkout);
+	})).catch(err => {
+		res.json(err);
+	});
+});
+
+// Route for updating workout
+router.put("/:id", (req, res) => {
+	db.Workout.findOneAndUpdate(
+		{ _id: req.params.id },
+		{
+			$inc: { totalDuration: req.body.duration },
+			$push: { exercises: req.body }
+		},
+		{ new: true }).then(dbWorkout => {
+			res.json(dbWorkout);
+		}).catch(err => {
+			res.json(err);
+		});
+});
+
+// Route for getting workouts based on specific range.
+router.get("/range", (req, res) => {
+	db.Workout.find({})
+		.then(dbWorkout => {
+			res.json(dbWorkout);
+		}).catch(err => {
+			res.json(err);
+		});
+});
+
+module.exports = router;
